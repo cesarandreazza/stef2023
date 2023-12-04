58bc2203890f936d6ac814cca7fdd27a829a522d
cesarandreazza <cesar.andreazza@gmail.com>
2023-12-04 16:59:07 -0300

    Login inicial (#3)
    * Login inicial
    
    * Alterar nome do metodo

---
+++ SolutionDosGuri/WebAppTreino/Controllers/LoginController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAppTreino.RequestModels;
+using WebAppTreino.ResponseModel;
+
+namespace WebAppTreino.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        [HttpPost(Name = "login")]
+        public IActionResult Login([FromBody] UserRequest user)
+        {
+            return new JsonResult(new UserResponse() { Id = 0, Name= user.UserName, Email = user.Password}); 
+        }
+    }
+}
---
+++ SolutionDosGuri/WebAppTreino/ResponseModel/UserResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace WebAppTreino.ResponseModel
+{
+    public class UserResponse
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+    }
+}
